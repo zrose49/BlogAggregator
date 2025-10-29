cdddd031a9e1fc885b525fd34a5bba342d7a9754
zrose49 <zxr110230@gmail.com>
2025-10-29 17:17:11 -0500

    fixed followFeed command to use current user from readConfig

--- src/commands/follow.ts
+++ src/commands/follow.ts
@@ -1,11 +1,14 @@
+import { readConfig } from "src/config";
 import { createFeedFollow } from "src/lib/db/queries/feed_follow";
 import { getFeedIdByURL, getFeeds, getUserIdByFeedID } from "src/lib/db/queries/feeds";
+import { getUser } from "src/lib/db/queries/users";
 
 
 export async function followFeed(cmdName: string, url: string) {
 
 const feedId = (await getFeedIdByURL(url)).id;
-const userId = (await getUserIdByFeedID(feedId)).user_id;
+const currentUser = readConfig().currentUserName;
+const userId = (await getUser(currentUser)).id;
 
 let result = await createFeedFollow(feedId,userId);
 
